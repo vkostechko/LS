264b26e4824465008d0430eec4a159d300ca8f9e
Viachaslau Kastsechka <viacheslav.kostechko@gmail.com>
2022-05-15 16:10:13 +0200

    created project

---
+++ LS/LS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LS
+//
+//  Created by Viachaslau Kastsechka on 5/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
